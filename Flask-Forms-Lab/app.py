623e5535de4556396ac8ecf31d658f37cf220520
sarahna24-meet <139690083+sarahna24-meet@users.noreply.github.com>
2023-07-19 12:14:46 +0300

    its a mess

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -10,14 +10,19 @@ app = Flask(  # Create a flask app
 
 username = "llo2ay"
 password = "123"
-facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
+facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina","Sobhe"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/', methods = ['POST', 'GET'])  # '/' for the default page
 def login():
-  return render_template('login.html')
+	if request.method == 'POST':
+	 if username == request.form['username'] and password == request.form['password']:
+	    return render_template('home.html')
+	redirect(url_for('home'))
   
-
+@app.route('/')
+def home():
+	 return render_template("home.html", friends = facebook_friends)
 
 
 if __name__ == "__main__":  # Makes sure this is the main process
